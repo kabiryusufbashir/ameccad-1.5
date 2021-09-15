15a2ed239099d58380d506bee0a8cc7351b58f95
Piccolo <kabiryusufbashir@gmail.com>
2021-09-15 17:12:33 +0100

    Releasing Beta version

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -59,7 +59,7 @@
             <hr>
             <li class="py-3 px-8">
                 <a href="/team" class="flex justify-between items-center">
-                    <span>Login</span>
+                    <span>Team</span>
                     &nbsp;&nbsp;
                     <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                 </a>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="text-center my-3">
-        <img :src="require('../assets/images/' + path + '')" :alt="name" class="w-56 mx-auto rounded-full">
+        <!-- <img :src="require('../assets/images/' + path + '')" :alt="name" class="w-56 mx-auto rounded-full"> -->
+        <img :src="require(`../assets/images/${path}`)" :alt="name" class="w-56 mx-auto rounded-full">
             <h5 class="mt-3 text-center text-xl font-bold">{{ name }}</h5>
             <p>{{ rank }}</p>
     </div>
