2f0444d1edd3ec066bca0c24eb56675a1da0e1cb
Piccolo <kabiryusufbashir@gmail.com>
2021-09-15 16:53:39 +0100

    Worked on the Iframe

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -34,43 +34,43 @@
     <div id="nav" class="w-full fixed h-screen z-30 hidden bg-white py-8">
         <div class="list-none p-2 text-xl border-t bg-white pt-20">
             <li class="py-3 px-8">
-                <router-link to="/" class="flex justify-between items-center">
+                <a href="/" class="flex justify-between items-center">
                     <span>Home</span>
                     &nbsp;&nbsp;
                     <span><i class="fas fa-home text-3xl"></i></span>
-                </router-link>
+                </a>
             </li>
             <hr>
             <li class="py-3 px-8" id="phone_nav_caret">
-                <router-link to="/about" class="flex justify-between items-center">
+                <a href="/about" class="flex justify-between items-center">
                     <span>About Us</span>
                     &nbsp;&nbsp;
                     <span><svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg></span>
-                </router-link>
+                </a>
             </li>
             <hr>
             <li class="py-3 px-8">
-                <router-link to="/courses" class="flex justify-between border-b-1 items-center">
+                <a href="/courses" class="flex justify-between border-b-1 items-center">
                     <span>Courses</span>
                     &nbsp;&nbsp;
                     <span><i class="fas fa-dot-circle text-3xl"></i></span>
-                </router-link>
+                </a>
             </li>
             <hr>
             <li class="py-3 px-8">
-                <router-link to="/team" class="flex justify-between items-center">
+                <a href="/team" class="flex justify-between items-center">
                     <span>Login</span>
                     &nbsp;&nbsp;
                     <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                </router-link>
+                </a>
             </li>
             <hr>
             <li class="py-3 px-8">
-                <router-link to="/contact" class="flex justify-between items-center">
+                <a href="/contact" class="flex justify-between items-center">
                     <span>Contact Us</span>
                     &nbsp;&nbsp;
                     <span><i class="fas fa-home text-3xl"></i></span>
-                </router-link>
+                </a>
             </li>
         </div>
     </div>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="px-4 md:px-24 py-24">
-        <div class="w-1/2 mx-auto my-6">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902.729951735581!2d8.542666114760427!3d11.993178491503048!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x11ae8173cc7b80a1%3A0xefc188a85e8c2293!2sAMEC%20CAD%20Training%20Centre!5e0!3m2!1sen!2sng!4v1612965868185!5m2!1sen!2sng" width="100%" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <div class="w-full md:w-1/2 mx-auto my-6">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902.729951735581!2d8.542666114760427!3d11.993178491503048!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x11ae8173cc7b80a1%3A0xefc188a85e8c2293!2sAMEC%20CAD%20Training%20Centre!5e0!3m2!1sen!2sng!4v1612965868185!5m2!1sen!2sng" width="100%" class="h-64 md:h-96" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         </div>
-        <div class="w-1/2 text-left mx-auto">
+        <div class="w-full md:w-1/2 text-left mx-auto">
             <div class="md:grid grid-cols-2 gap-4 items-center justify-around">
                 <div>
                     <i class="fas fa-phone"></i><br>
